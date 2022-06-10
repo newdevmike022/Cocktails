8e3a1a2eb205f12e1a991ded04f0152664256bee
Michael Dodson <95949089+newdevmike022@users.noreply.github.com>
2022-06-10 10:31:42 -0500

    Branch - 3
    Completed and styled the About and Error pages.

--- drinks/src/pages/Error.js
+++ drinks/src/pages/Error.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div></div>;
+  return (
+    <section className="section error-page">
+      <div className="error-container">
+        <h1>Oops! It's a dead end!</h1>
+        <Link to="/" className="btn btn-primary">
+          Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
